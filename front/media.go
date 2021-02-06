8c93ec0de6630137118194dd6376b2ba68c1944e
Stanislav Mursalov <shamaner.17@gmail.com>
2021-02-06 22:45:30 +0300

    Shaman

--- front/media.go
+++ front/media.go
@@ -2,11 +2,12 @@ package front
 
 import (
 	"fmt"
-	"github.com/massarakhsh/lik"
-	"github.com/massarakhsh/lik/likdom"
 	"io/ioutil"
 	"sort"
 	"strings"
+
+	"github.com/massarakhsh/lik"
+	"github.com/massarakhsh/lik/likdom"
 )
 
 var dirMain = "media"
@@ -28,14 +29,14 @@ func (rule *DataRule) MediaShow(sx int, sy int) likdom.Domer {
 	tbl := likdom.BuildTableClassId("fill", "win_media")
 	tbl.BuildTrTd().AppendItem(rule.mediaCommands())
 	lev := len(lik.PathToNames(rule.ItPage.DirPath))
-	tbl.BuildTrTd().AppendItem(rule.mediaPath(sx, lev * 24))
+	tbl.BuildTrTd().AppendItem(rule.mediaPath(sx, lev*24))
 	tbl.BuildTrTd().BuildString("<hr>")
-	dy := sy - 60 - lev * 24
+	dy := sy - 60 - lev*24
 	td := tbl.BuildTrTd(MakeSizes(sx, dy)...)
 	if rule.ItPage.IsLoad {
 		td.AppendItem(rule.mediaLoad(sx, dy))
 	} else {
-		dx := sx / 2 - BD
+		dx := sx/2 - BD
 		row := td.BuildTableClass("fill")
 		row.BuildTd(MakeSizes(dx, dy)...).AppendItem(rule.mediaBrowser(dx, dy))
 		row.BuildTd(MakeSizes(dx, dy)...).AppendItem(rule.mediaImage(dx, dy))
@@ -81,7 +82,7 @@ func (rule *DataRule) MediaReImage() {
 func (rule *DataRule) mediaImage(sx int, sy int) likdom.Domer {
 	rule.ItPage.ImageSize = Size{sx, sy}
 	div := likdom.BuildDivClassId("win_visual", "win_preview")
-	div.AppendItem(rule.VisualSource(sx - 8, sy - 8, "/" + dirMain + rule.ItPage.FilePath))
+	div.AppendItem(rule.VisualSource(sx-8, sy-8, "/"+dirMain+rule.ItPage.FilePath))
 	return div
 }
 
@@ -112,7 +113,7 @@ func (rule *DataRule) mediaPath(sx int, sy int) likdom.Domer {
 			path = "/" + strings.Join(names[:nl], "/")
 		}
 		link := LinkTextProc("cmd cmdd", name, fmt.Sprintf("media_path('%s')", path))
-		row.BuildTd(fmt.Sprintf("width=%dpx", sx - nl * 24), fmt.Sprintf("colspan=%d", ml+1-nl)).AppendItem(link)
+		row.BuildTd(fmt.Sprintf("width=%dpx", sx-nl*24), fmt.Sprintf("colspan=%d", ml+1-nl)).AppendItem(link)
 	}
 	return tbl
 }
@@ -120,7 +121,7 @@ func (rule *DataRule) mediaPath(sx int, sy int) likdom.Domer {
 func (rule *DataRule) mediaFiles(sx int, sy int) likdom.Domer {
 	tbl := likdom.BuildTable("width=100%")
 	if files, err := ioutil.ReadDir(dirMain + rule.ItPage.DirPath); err == nil {
-		sort.Slice(files, func(i,j int) bool {
+		sort.Slice(files, func(i, j int) bool {
 			return files[i].Name() <= files[j].Name()
 		})
 		for fase := 0; fase < 2; fase++ {
@@ -128,7 +129,7 @@ func (rule *DataRule) mediaFiles(sx int, sy int) likdom.Domer {
 				if name := file.Name(); name != "" {
 					full := rule.ItPage.DirPath + "/" + name
 					if fase == 0 && file.IsDir() {
-						link := LinkTextProc("cmd cmdd", "[" + name + "]", fmt.Sprintf("media_path('%s')", full))
+						link := LinkTextProc("cmd cmdd", "["+name+"]", fmt.Sprintf("media_path('%s')", full))
 						tbl.BuildTrTd("colspan=2").AppendItem(link)
 					} else if fase == 1 && !file.IsDir() {
 						row := tbl.BuildTr()
@@ -184,7 +185,7 @@ func (rule *DataRule) mediaDoUpload() {
 	if buffers := rule.GetBuffers(); buffers != nil {
 		rule.ItPage.LoadSync.Lock()
 		for name, val := range buffers {
-			pot := Pot{ Name: name, Data: val }
+			pot := Pot{Name: name, Data: val}
 			rule.ItPage.Upload = append(rule.ItPage.Upload, pot)
 		}
 		rule.ItPage.LoadSync.Unlock()
@@ -193,7 +194,7 @@ func (rule *DataRule) mediaDoUpload() {
 }
 
 func (rule *DataRule) mediaDoStore() {
-	for _,pot := range rule.ItPage.Upload {
+	for _, pot := range rule.ItPage.Upload {
 		file := lik.Transliterate(pot.Name)
 		path := dirMain + rule.ItPage.DirPath + "/" + file
 		_ = ioutil.WriteFile(path, pot.Data, 0666)
@@ -208,4 +209,3 @@ func (rule *DataRule) mediaDoCancel() {
 	rule.ItPage.Upload = []Pot{}
 	rule.MediaReShow()
 }
-
